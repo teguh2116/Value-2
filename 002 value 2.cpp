86cc5074d5f38ac08408c57ef2687efa6541eed8
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 06:20:29 +0700

    Add files via upload

---
+++ 002 value 2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int jumlah (int x, int y);
+
+int main()
+{
+    int a,b,t;
+    a= 5; b=2;
+    t=jumlah (a,b);
+    printf("%d,t");
+
+}
+int jumlah (int x, int y)
+{
+    int h;
+    h=x+y;
+    return (0);
+}
